7527ba79e4ffc123a5ae40d58a0377f6fc30c81e
Pegasian0 <acomoreanu@yahoo.com>
2022-02-18 00:00:47 +0200

    Add feature
    2D Combat skill - Comoreanu Alessandro

---
+++ UnityClient/Assets/_DEV/feature_combat_skill/scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D body;
+    [SerializeField] private float speed = 1f;
+    [SerializeField] private float jump = 1f;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetBool("isAttacking", true);
+        else anim.SetBool("isAttacking", false);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jump);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+    public bool canAttack()
+    {
+        return grounded;
+    }
+}
